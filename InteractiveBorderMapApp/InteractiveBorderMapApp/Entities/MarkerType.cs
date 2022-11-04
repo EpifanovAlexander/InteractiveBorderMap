948b088b8cf7cc484dffdc7390f5794de163310b
Alexander Epifanov <alexlaw00@mail.ru>
2022-11-04 19:02:19 +0300

    Updated ui for showing markers

---
+++ InteractiveBorderMapApp/InteractiveBorderMapApp/Entities/MarkerType.cs
@@ -0,0 +1,9 @@
+﻿namespace InteractiveBorderMapApp.Entities
+{
+    public static class MarkerType
+    {
+        public const string INCLUDE = "include";
+        public const string EXCLUDE = "exclude";
+        public const string DISCUSS = "discuss";
+    }
+}
