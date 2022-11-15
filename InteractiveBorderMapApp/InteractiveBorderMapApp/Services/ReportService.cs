cb2cbb70ce0624b826a95d3fc4c2621a06653068
Alexander Epifanov <alexlaw00@mail.ru>
2022-11-15 16:19:17 +0300

    Added reportService

---
+++ InteractiveBorderMapApp/InteractiveBorderMapApp/Services/ReportService.cs
@@ -0,0 +1,15 @@
+﻿using InteractiveBorderMapApp.Controllers;
+using System.IO;
+
+namespace InteractiveBorderMapApp.Services
+{
+    public class ReportService
+    {
+        private string webRootPath = HomeController.WebRootPath;
+        public FileStream GetReport(string reportId)
+        {
+            var path = Path.Combine(webRootPath, @$"reports/{reportId}");
+            return File.OpenRead(path);
+        }
+    }
+}
